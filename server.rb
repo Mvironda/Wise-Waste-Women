8eb04f29cc3a31add6e9ac2874e82a01a8853d62
Mvironda <vironda.madeleine@gmail.com>
2017-11-16 20:27:48 +0000

    deleted unnecessary html

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'csv'
+require 'twitter'
 
 get '/' do
 
@@ -12,4 +13,19 @@ get '/boroughs/:borough_name' do
 	@borough = params[:borough_name]
 
 	erb :borough
+end
+
+twitter_api = Twitter::REST::Client.new do |config|
+config.consumer_key = "ti9FkhNaXjvBmt6kwnf8DbQEN"
+config.consumer_secret = "HrrSzcOH7rBKvPrmQEpzMUovtfR4HEeD0N0SIjCcpiraZmepWo"
+config.access_token = "836261833946382336-VszT2RSrGATyZVPaa1MlZ2FBajiBiL7"
+config.access_token_secret = "JPl5BogxaMqW42WAVU7VXCuXkln1IhwxxeTV72WX9THo3"
+end
+
+get '/:search_query' do
+	@search_query = params[:search_query]
+	@tweets = client.search(@search_query)
+
+	erb :tweet_list
+
 end
