4246448a8c383b2f2421c287c5153d5bce57623b
Mvironda <vironda.madeleine@gmail.com>
2017-11-20 19:28:42 +0000

    Final touches

--- server.rb
+++ server.rb
@@ -15,17 +15,16 @@ get '/boroughs/:borough_name' do
 	erb :borough
 end
 
-twitter_api = Twitter::REST::Client.new do |config|
-config.consumer_key = "ti9FkhNaXjvBmt6kwnf8DbQEN"
-config.consumer_secret = "HrrSzcOH7rBKvPrmQEpzMUovtfR4HEeD0N0SIjCcpiraZmepWo"
-config.access_token = "836261833946382336-VszT2RSrGATyZVPaa1MlZ2FBajiBiL7"
-config.access_token_secret = "JPl5BogxaMqW42WAVU7VXCuXkln1IhwxxeTV72WX9THo3"
-end
+##config.consumer_key = "ti9FkhNaXjvBmt6kwnf8DbQEN"
+##config.consumer_secret = "HrrSzcOH7rBKvPrmQEpzMUovtfR4HEeD0N0SIjCcpiraZmepWo"
+##config.access_token = "836261833946382336-VszT2RSrGATyZVPaa1MlZ2FBajiBiL7"
+##config.access_token_secret = "JPl5BogxaMqW42WAVU7VXCuXkln1IhwxxeTV72WX9THo3"
+##end
 
-get '/:search_query' do
-	@search_query = params[:search_query]
-	@tweets = client.search(@search_query)
+##get '/:search_query' do
+##	@search_query = params[:search_query]
+##	@tweets = client.search(@search_query)
 
-	erb :tweet_list
+##	erb :tweet_list
 
-end
+## end
